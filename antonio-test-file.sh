9e950535afba273f5b7d6bee4f182db71be05ecc
Antonio Feijao <2548084+AntonioFeijaoUK@users.noreply.github.com>
2022-10-18 21:32:29 -0400

    Create antonio-test-file.sh

---
+++ antonio-test-file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Antonio test file"
+
+echo "Testing downloading or uploading a random file..."
+
+echo "This file is not malicious"
+
+echo "but could be.... it is in a random repository... well, Antonio Feijao UK's repository".
+
+echo "End of test..."
+
+
